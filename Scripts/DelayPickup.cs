de1141cdf9b7ea166c2c086c7b8000a017bef908
em-twines <emdelbridge@gmail.com>
2023-03-07 17:54:33 -0500

    scripts from lookingGlass video game, premiered 2020

---
+++ Scripts/DelayPickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class DelayPickup : MonoBehaviour
+{
+    public int delay = 114; 
+    void Start()
+    {
+        Collider sledgeHammer = gameObject.GetComponent<Collider>();
+        sledgeHammer.isTrigger = false;
+
+        Invoke("IsTrigger", delay);
+        //114
+    }
+
+    void IsTrigger()
+    {
+        Collider sledgeHammer = gameObject.GetComponent<Collider>();
+        sledgeHammer.isTrigger = true;
+        //Outputto console the GameObject’s trigger state
+      //  Debug.Log("Trigger On : " + sledgeHammer.isTrigger);
+    }
+
+    
+
+}
+
+
+
+      
